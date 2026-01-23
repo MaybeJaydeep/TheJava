f1c27052e08530bfc1c9125e2b0af231993430c1
MaybeJaydeep <jaydeep.badal.pro@gmail.com>
2026-01-23 14:07:31 +0530

    Practice for throw, throws

---
+++ Practice/Throw_Throws/Division.java
@@ -0,0 +1,19 @@
+package Throw_Throws;
+
+import java.util.Scanner;
+
+public class Division {
+    public double division() throws ArithmeticException, NumberFormatException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1:");
+        int n1 = sc.nextInt();
+        System.out.println("Enter number 2:");
+        int n2 = sc.nextInt();
+
+        if(n2 == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+        return n1/n2;
+    }
+}
---
+++ Practice/Throw_Throws/ThrowsDemo.java
@@ -0,0 +1,10 @@
+package Throw_Throws;
+
+class ThrowsDemo{
+    public static void main(String[] args) {
+        Division div = new Division();
+        double output = div.division();
+        System.out.println(output);
+
+    }
+}
