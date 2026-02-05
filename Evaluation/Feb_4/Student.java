028f38a2c1be4f0d6ef72e35158ec9d99cd46243
MaybeJaydeep <jaydeep.badal.pro@gmail.com>
2026-02-05 17:47:23 +0530

    Evaulation 1st problem solved...

---
+++ Evaluation/Feb_4/Student.java
@@ -0,0 +1,45 @@
+package Evaluation.Feb_4;
+
+import java.util.Scanner;
+
+public class Student {
+
+    private int studentID;
+    private String name;
+    private String grade;
+
+    static Scanner input = new Scanner(System.in);
+
+    public Student() {}
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void insert() {
+        System.out.print("Enter Student ID: ");
+        studentID = input.nextInt();
+
+        System.out.print("Enter Student Name: ");
+        name = input.next();
+
+        System.out.print("Enter Student Grade: ");
+        grade = input.next();
+    }
+
+    public void updateDetails() {
+        System.out.print("Enter new name: ");
+        name = input.next();
+
+        System.out.print("Enter new grade: ");
+        grade = input.next();
+    }
+}
---
+++ Evaluation/Feb_4/StudentManagement.java
@@ -0,0 +1,53 @@
+package Evaluation.Feb_4;
+
+import java.util.Scanner;
+
+public class StudentManagement {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Service service = new Service();
+
+        while (true) {
+            System.out.println("\nStudent Management System");
+            System.out.println("1. Insert Student");
+            System.out.println("2. Display Student by ID");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Student student = new Student();
+                    student.insert();
+                    service.addStudent(student);
+                    break;
+
+                case 2:
+                    System.out.print("Enter ID: ");
+                    service.displayByID(sc.nextInt());
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID: ");
+                    service.updateStudentByID(sc.nextInt());
+                    break;
+
+                case 4:
+                    System.out.print("Enter ID: ");
+                    service.deleteStudentByID(sc.nextInt());
+                    break;
+
+                case 5:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
